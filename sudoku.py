0a6417a6d34570006dd2a7f871f370d6fa57adc9
Andrzej Woźniak <wozniwozniak@gmail.com>
2023-07-26 16:49:04 +0200

    add function solver

--- sudoku.py
+++ sudoku.py
@@ -48,51 +48,55 @@ class Cell:
         self.square_num = square.num
         self.value = value
 
+    def choice(self):
+        return np.intersect1d(np.intersect1d(np.array(self.row.num_options), np.array(self.col.num_options)),
+                              np.array(self.square.num_options))
+
     def draw_val(self):
         try:
-            self.value = random.choice(
-                np.intersect1d(np.intersect1d(np.array(self.row.num_options), np.array(self.col.num_options)),
-                               np.array(self.square.num_options)))
-            print(self.value)
+            self.value = random.choice(self.choice())
         except IndexError:
             return None
         self.row.num_options.remove(self.value)
         self.col.num_options.remove(self.value)
         self.square.num_options.remove(self.value)
 
+    def add_value(self, value):
+        if value is None:
+            return False
+        self.value = value
+        self.row.num_options.remove(self.value)
+        self.col.num_options.remove(self.value)
+        self.square.num_options.remove(self.value)
 
-rows = [Row(i) for i in range(1, 10)]
-cols = [Col(i) for i in range(1, 10)]
-squares = [Square(i) for i in range(1, 10)]
+    def del_value(self):
+        self.row.num_options.append(self.value)
+        self.row.num_options.sort()
+        self.col.num_options.append(self.value)
+        self.col.num_options.sort()
+        self.square.num_options.append(self.value)
+        self.square.num_options.sort()
+        index = np.where(self.choice() == self.value)[0][0]
+
+        self.value = None
+        if len(self.choice()) == index + 1:
+            return None
+        else:
+            return self.choice()[index+1]
+
+
+rows = [Row(i) for i in range(0, 9)]
+cols = [Col(i) for i in range(0, 9)]
+squares = [Square(i) for i in range(0, 9)]
 
 
 def square_indent(row, col):
-    r = row.num
-    c = col.num
-    if 1 <= r <= 3 and 1 <= c <= 3:
-        return 1
-    elif 1 <= r <= 3 and 4 <= c <= 6:
-        return 2
-    elif 1 <= r <= 3 and 7 <= c <= 9:
-        return 3
-    elif 4 <= r <= 6 and 1 <= c <= 3:
-        return 4
-    elif 4 <= r <= 6 and 4 <= c <= 6:
-        return 5
-    elif 4 <= r <= 6 and 7 <= c <= 9:
-        return 6
-    elif 7 <= r <= 9 and 1 <= c <= 3:
-        return 7
-    elif 7 <= r <= 9 and 4 <= c <= 6:
-        return 8
-    elif 7 <= r <= 9 and 7 <= c <= 9:
-        return 9
-
+    return (3 * (row.num // 3)) + (col.num // 3)
 
 cells = []
 for i in rows:
     for j in cols:
-        sq = squares[square_indent(i, j) - 1]
+        sq = squares[square_indent(i, j)]
         cell = Cell(i, j, sq)
         cells.append(cell)
         sq.add_cell(cell)
@@ -102,6 +106,31 @@ for i in rows:
 for i in [0, 4, 8]:
     for cell in squares[i].cells:
         cell.draw_val()
+
+for row in rows:
+    print([cell.value for cell in row.cells])
+
+none_cells = [cell for cell in cells if cell.value is None]
+# random.shuffle(none_cells)
+
+
+def solver(n):
+    if n == 54:
+        return True
+
+    elif len(none_cells[n].choice()) == 0:
+        if none_cells[n].value is not None:
+            none_cells[n].del_value()
+        return False
+
+    else:
+        none_cells[n].add_value(none_cells[n].choice()[0])
+        while solver(n+1) is False:
+            if none_cells[n].add_value(none_cells[n].del_value()) is False:
+                return False
+
+
+solver(0)
 
 for row in rows:
     print([cell.value for cell in row.cells])
